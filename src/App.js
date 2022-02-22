ae52a231cb6c1ce972f792cd98d09accef554b17
kazu-0226 <59648368+kazu-0226@users.noreply.github.com>
2022-02-22 13:09:26 +0900

    card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { Usercard } from "./components/organisms/user/UserCard";
 import "./styles.css";
+
+const user = {
+  name: "犬山 犬男",
+  image: "https://source.unsplash.com/NE0XGVKTmcA",
+  email: "1234@example.com",
+  phone: "090-1111-2222",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "https://github.co.jp/"
+};
 
 export default function App() {
   return (
@@ -10,6 +22,7 @@ export default function App() {
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput />
+      <Usercard user={user} />
     </div>
   );
 }
---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,35 @@
+import styled, { useTheme } from "styled-components";
+import { Card } from "../../atoms/card/Card";
+
+export const Usercard = (props) => {
+  const { user } = props;
+
+  return (
+    <Card>
+      <img height={160} width={160} src={user.image} alt={user.name} />
+      <p>{user.name}</p>
+      <SDl>
+        <dt>メール</dt>
+        <dd>{user.email}</dd>
+        <dt>TEL</dt>
+        <dd>{user.phone}</dd>
+        <dt>会社名</dt>
+        <dd>{user.company.name}</dd>
+        <dt>Web</dt>
+        <dd>{user.website}</dd>
+      </SDl>
+    </Card>
+  );
+};
+
+const SDl = styled.dl`
+  text-align: left;
+  margin-bottom: 0px;
+  dt {
+    float: left;
+  }
+  dd {
+    padding-left: 32px;
+    padding-bottom: 8px;
+  }
+`;
