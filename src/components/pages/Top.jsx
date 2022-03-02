3b773e23fc2a435f687b567bb554508e0e0e08b2
kazu-0226 <59648368+kazu-0226@users.noreply.github.com>
2022-03-02 17:36:21 +0900

    Context2

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,14 +1,22 @@
 import styled from "styled-components";
 import { useHistory } from "react-router-dom";
 import { SecondaryButton } from "../atoms/button/SecondaryButton";
+import { useContext } from "react";
+import { UserContext } from "../../providers/UserProvider";
 
 export const Top = () => {
   const history = useHistory();
+  const { setUserInfo } = useContext(UserContext);
 
-  const onCllickAdmin = () =>
-    history.push({ pathname: "/users", state: { isAdmin: true } });
-  const onCllickGeneral = () =>
-    history.push({ pathname: "/users", state: { isAdmin: false } });
+  const onCllickAdmin = () => {
+    setUserInfo({ isAdmin: true });
+    history.push("/users");
+  };
+  const onCllickGeneral = () => {
+    setUserInfo({ isAdmin: false });
+    history.push("/users");
+  };
+
   return (
     <SContainer>
       <h2>TOPページ</h2>
