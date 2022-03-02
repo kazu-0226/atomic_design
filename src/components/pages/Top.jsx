519586aef194cbbdab0522bfae2a43170a2edbdd
kazu-0226 <59648368+kazu-0226@users.noreply.github.com>
2022-03-02 16:32:45 +0900

    state管理

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,9 +1,21 @@
 import styled from "styled-components";
+import { useHistory } from "react-router-dom";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
 
 export const Top = () => {
+  const history = useHistory();
+
+  const onCllickAdmin = () =>
+    history.push({ pathname: "/users", state: { isAdmin: true } });
+  const onCllickGeneral = () =>
+    history.push({ pathname: "/users", state: { isAdmin: false } });
   return (
     <SContainer>
       <h2>TOPページ</h2>
+      <SecondaryButton onClick={onCllickAdmin}>管理者ユーザー</SecondaryButton>
+      <br />
+      <br />
+      <SecondaryButton onClick={onCllickGeneral}>一般ユーザー</SecondaryButton>
     </SContainer>
   );
 };
